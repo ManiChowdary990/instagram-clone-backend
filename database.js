9d8e28d86ae14c314c60223a0a7b64334728f032
ManiChowdary990 <188636107+ManiChowdary990@users.noreply.github.com>
2025-02-12 02:18:43 +0530

    Update database.js

--- database.js
+++ database.js
@@ -2,10 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true, // ✅ This is ignored in MongoDB 4.0+, but safe to keep
-      useUnifiedTopology: true, // ✅ This is ignored in MongoDB 4.0+, but safe to keep
-    });
+    await mongoose.connect(MONGO_URI);
+
 
     console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
