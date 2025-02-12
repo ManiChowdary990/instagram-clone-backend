b895dddd3891a59f5c3170c64a1e5ad9adfea4db
MANIKANTA <188636107+ManiChowdary990@users.noreply.github.com>
2025-02-12 05:54:56 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,14 +3,16 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
-    fullName: { type: String, required: true },
-    password: { type: String, required: true },
-  },
-  { timestamps: true }
-);
-
+     email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
 // Hash password before saving to database
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
